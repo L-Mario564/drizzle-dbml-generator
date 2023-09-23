4af1c1ad0f422526f0d83b37fd927062bd19faf6
Mario564 <ka.mario564@gmail.com>
2023-09-23 11:34:24 -0600

    Finish MySQL tests

--- src/__tests__/mysql/_mysql.test.ts
+++ src/__tests__/mysql/_mysql.test.ts
@@ -3,6 +3,7 @@ import { compareContents } from '../utils';
 import { mysqlGenerate } from '~/generators';
 //import { relations } from 'drizzle-orm';
 import { bigint, binary, boolean, char, datetime, decimal, double, float, foreignKey, index, int, mediumint, mysqlEnum, mysqlTable, primaryKey, real, serial, smallint, text, time, timestamp, tinyint, unique, uniqueIndex, varbinary, varchar, year } from 'drizzle-orm/mysql-core';
+import { relations } from 'drizzle-orm';
 
 const pathPrefix = './src/__tests__/mysql/';
 
@@ -137,161 +138,161 @@ async function indexesTest() {
   expect(result).toBe(true);
 }
 
-// async function rqbTest() {
-//   const users = pgTable('users', {
-//     id: serial('id').primaryKey(),
-//     configId: integer('config_id').references(() => userConfig.id, {
-//       onDelete: 'set null'
-//     })
-//   });
-//   const usersRelations = relations(users, ({ one, many }) => ({
-//     userConfig: one(userConfig, {
-//       fields: [users.configId],
-//       references: [userConfig.id]
-//     }),
-//     sells: many(items)
-//   }));
+async function rqbTest() {
+  const users = mysqlTable('users', {
+    id: serial('id').primaryKey(),
+    configId: int('config_id').references(() => userConfig.id, {
+      onDelete: 'set null'
+    })
+  });
+  const usersRelations = relations(users, ({ one, many }) => ({
+    userConfig: one(userConfig, {
+      fields: [users.configId],
+      references: [userConfig.id]
+    }),
+    sells: many(items)
+  }));
 
-//   const userConfig = pgTable('user_config', {
-//     id: serial('id').primaryKey()
-//   });
-//   const userConfigRelations = relations(userConfig, ({ one }) => ({
-//     user: one(users)
-//   }));
+  const userConfig = mysqlTable('user_config', {
+    id: serial('id').primaryKey()
+  });
+  const userConfigRelations = relations(userConfig, ({ one }) => ({
+    user: one(users)
+  }));
 
-//   const items = pgTable('items', {
-//     id: serial('id').primaryKey(),
-//     soldById: integer('sold_by_id')
-//       .notNull()
-//       .references(() => users.id, {
-//         onDelete: 'cascade'
-//       })
-//   });
-//   const itemsRelations = relations(items, ({ one }) => ({
-//     soldBy: one(users, {
-//       fields: [items.soldById],
-//       references: [users.id]
-//     })
-//   }));
+  const items = mysqlTable('items', {
+    id: serial('id').primaryKey(),
+    soldById: int('sold_by_id')
+      .notNull()
+      .references(() => users.id, {
+        onDelete: 'cascade'
+      })
+  });
+  const itemsRelations = relations(items, ({ one }) => ({
+    soldBy: one(users, {
+      fields: [items.soldById],
+      references: [users.id]
+    })
+  }));
 
-//   const schema = {
-//     users,
-//     usersRelations,
-//     userConfig,
-//     userConfigRelations,
-//     items,
-//     itemsRelations
-//   };
-//   const out = `${pathPrefix}relations.generated.dbml`;
-//   const relational = true;
-//   pgGenerate({ schema, out, relational });
+  const schema = {
+    users,
+    usersRelations,
+    userConfig,
+    userConfigRelations,
+    items,
+    itemsRelations
+  };
+  const out = `${pathPrefix}relations.generated.dbml`;
+  const relational = true;
+  mysqlGenerate({ schema, out, relational });
 
-//   const result = await compareContents(out);
-//   expect(result).toBe(true);
-// }
+  const result = await compareContents(out);
+  expect(result).toBe(true);
+}
 
-// async function realTest() {
-//   const users = pgTable('users', {
-//     id: serial('id').primaryKey(),
-//     registeredAt: timestamp('registered_at').notNull().defaultNow(),
-//     username: varchar('username', { length: 16 }).notNull().unique('uq_users_username'),
-//     bio: text('bio'),
-//     hasBlue: boolean('has_blue').notNull().default(false)
-//   });
+async function realTest() {
+  const users = mysqlTable('users', {
+    id: serial('id').primaryKey(),
+    registeredAt: timestamp('registered_at').notNull().defaultNow(),
+    username: varchar('username', { length: 16 }).notNull().unique('uq_users_username'),
+    bio: text('bio'),
+    hasBlue: boolean('has_blue').notNull().default(false)
+  });
 
-//   const usersRelations = relations(users, ({ many }) => ({
-//     followers: many(followers, { relationName: 'user_followers' }),
-//     following: many(followers, { relationName: 'user_follows' }),
-//     tweets: many(tweets),
-//     likes: many(likes)
-//   }));
+  const usersRelations = relations(users, ({ many }) => ({
+    followers: many(followers, { relationName: 'user_followers' }),
+    following: many(followers, { relationName: 'user_follows' }),
+    tweets: many(tweets),
+    likes: many(likes)
+  }));
 
-//   const followers = pgTable(
-//     'followers',
-//     {
-//       userId: integer('user_id')
-//         .notNull()
-//         .references(() => users.id),
-//       followsUserId: integer('follows_user_id')
-//         .notNull()
-//         .references(() => users.id)
-//     },
-//     (followers) => ({
-//       pk: primaryKey(followers.userId, followers.followsUserId)
-//     })
-//   );
+  const followers = mysqlTable(
+    'followers',
+    {
+      userId: int('user_id')
+        .notNull()
+        .references(() => users.id),
+      followsUserId: int('follows_user_id')
+        .notNull()
+        .references(() => users.id)
+    },
+    (followers) => ({
+      pk: primaryKey(followers.userId, followers.followsUserId)
+    })
+  );
 
-//   const followersRelations = relations(followers, ({ one }) => ({
-//     user: one(users, {
-//       fields: [followers.userId],
-//       references: [users.id],
-//       relationName: 'user_followers'
-//     }),
-//     followsUser: one(users, {
-//       fields: [followers.followsUserId],
-//       references: [users.id],
-//       relationName: 'user_follows'
-//     })
-//   }));
+  const followersRelations = relations(followers, ({ one }) => ({
+    user: one(users, {
+      fields: [followers.userId],
+      references: [users.id],
+      relationName: 'user_followers'
+    }),
+    followsUser: one(users, {
+      fields: [followers.followsUserId],
+      references: [users.id],
+      relationName: 'user_follows'
+    })
+  }));
 
-//   const tweets = pgTable('tweets', {
-//     id: serial('id').primaryKey(),
-//     postedAt: timestamp('posted_at').notNull().defaultNow(),
-//     content: text('content').notNull(),
-//     postedById: integer('posted_by_id')
-//       .notNull()
-//       .references(() => users.id)
-//   });
+  const tweets = mysqlTable('tweets', {
+    id: serial('id').primaryKey(),
+    postedAt: timestamp('posted_at').notNull().defaultNow(),
+    content: text('content').notNull(),
+    postedById: int('posted_by_id')
+      .notNull()
+      .references(() => users.id)
+  });
 
-//   const tweetsRelations = relations(tweets, ({ one }) => ({
-//     postedBy: one(users, {
-//       fields: [tweets.postedById],
-//       references: [users.id]
-//     })
-//   }));
+  const tweetsRelations = relations(tweets, ({ one }) => ({
+    postedBy: one(users, {
+      fields: [tweets.postedById],
+      references: [users.id]
+    })
+  }));
 
-//   const likes = pgTable(
-//     'likes',
-//     {
-//       likedTweetId: integer('liked_tweet_id')
-//         .notNull()
-//         .references(() => tweets.id),
-//       likedById: integer('liked_by_id')
-//         .notNull()
-//         .references(() => users.id)
-//     },
-//     (likes) => ({
-//       pk: primaryKey(likes.likedById, likes.likedTweetId)
-//     })
-//   );
+  const likes = mysqlTable(
+    'likes',
+    {
+      likedTweetId: int('liked_tweet_id')
+        .notNull()
+        .references(() => tweets.id),
+      likedById: int('liked_by_id')
+        .notNull()
+        .references(() => users.id)
+    },
+    (likes) => ({
+      pk: primaryKey(likes.likedById, likes.likedTweetId)
+    })
+  );
 
-//   const likesRelations = relations(likes, ({ one }) => ({
-//     likedTweet: one(tweets, {
-//       fields: [likes.likedTweetId],
-//       references: [tweets.id]
-//     }),
-//     likedBy: one(users, {
-//       fields: [likes.likedById],
-//       references: [users.id]
-//     })
-//   }));
+  const likesRelations = relations(likes, ({ one }) => ({
+    likedTweet: one(tweets, {
+      fields: [likes.likedTweetId],
+      references: [tweets.id]
+    }),
+    likedBy: one(users, {
+      fields: [likes.likedById],
+      references: [users.id]
+    })
+  }));
 
-//   const schema = {
-//     users,
-//     usersRelations,
-//     followers,
-//     followersRelations,
-//     tweets,
-//     tweetsRelations,
-//     likes,
-//     likesRelations
-//   };
-//   const out = `${pathPrefix}real.generated.dbml`;
-//   pgGenerate({ schema, out });
+  const schema = {
+    users,
+    usersRelations,
+    followers,
+    followersRelations,
+    tweets,
+    tweetsRelations,
+    likes,
+    likesRelations
+  };
+  const out = `${pathPrefix}real.generated.dbml`;
+  mysqlGenerate({ schema, out });
 
-//   const result = await compareContents(out);
-//   expect(result).toBe(true);
-// }
+  const result = await compareContents(out);
+  expect(result).toBe(true);
+}
 
 describe('MySQL dialect tests', () => {
   it('Outputs all native types', typesTest);
@@ -299,6 +300,6 @@ describe('MySQL dialect tests', () => {
   it('Outputs an inline foreign key', inlineFkTest);
   it('Outputs a foreign key', fkTest);
   it('Outputs all indexes', indexesTest);
-  // it('Outputs relations written with the RQB API', rqbTest);
-  // it('Outputs the result of a more "realistic" schema', realTest);
+  it('Outputs relations written with the RQB API', rqbTest);
+  it('Outputs the result of a more "realistic" schema', realTest);
 });
